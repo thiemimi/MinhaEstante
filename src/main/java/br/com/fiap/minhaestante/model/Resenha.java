aca71fd51131696ba6d4158082ada7efa7a22872
thiemimi <thiemi.h.favaro@gmail.com>
2024-03-06 11:07:24 -0300

    Criação do arquivo Resenha.java

---
+++ src/main/java/br/com/fiap/minhaestante/model/Resenha.java
@@ -0,0 +1,10 @@
+package br.com.fiap.minhaestante.model;
+
+public record Resenha(Long id, String nome, String icone) {
+    
+
+}
+
+// criar um record no java vai definir uma estrutura de dados que tem seus atributos, seus métodos de 
+// acessos e construtores
+// record é IMUTÁVEL 
