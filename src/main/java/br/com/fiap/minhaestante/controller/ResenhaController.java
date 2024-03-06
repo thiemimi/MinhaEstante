ef9862d6091d2f2011ff3085e2b7dcc362d930e9
thiemimi <thiemi.h.favaro@gmail.com>
2024-03-06 11:06:51 -0300

    alterações

--- src/main/java/br/com/fiap/minhaestante/controller/ResenhaController.java
+++ src/main/java/br/com/fiap/minhaestante/controller/ResenhaController.java
@@ -1,28 +1,47 @@
 package br.com.fiap.minhaestante.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.fiap.minhaestante.model.Resenha;
+
+
 @Controller
 public class ResenhaController {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+    
+    List<Resenha> repository = new ArrayList<>();
+
     //localhost
     @RequestMapping(
         method = RequestMethod.GET, 
         path= "/resenha", 
         produces ="application/json")
     @ResponseBody
-    public String index(){
-        return 
-        """   
-            [ 
-                { 
-                    "id": 1,
-                    "nome": "Resenha",
-                    "icone": "Resenha"
-                }
-            ]
-        """;
+    public List<Resenha> index(){
+        return repository;
     }
+
+    @RequestMapping(
+        method = RequestMethod.POST,
+        path = "/resenha",
+        produces = "application/json",
+        consumes = "application/json"
+    )
+    @ResponseBody
+    public void create(@RequestBody String resenha){ //binding
+        log.info("cadastrando categoria {}", resenha);
+        repository.add(new Resenha(1L, "Resenha", "Nome do Livro"));
+    }
+
+
 }
