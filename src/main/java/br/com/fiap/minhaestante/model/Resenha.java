406d78c4a595bc13ae48ae5cfbea58dce0be520d
thiemimi <thiemi.h.favaro@gmail.com>
2024-03-07 08:38:06 -0300

    alterações em resenha.java

--- src/main/java/br/com/fiap/minhaestante/model/Resenha.java
+++ src/main/java/br/com/fiap/minhaestante/model/Resenha.java
@@ -1,7 +1,13 @@
 package br.com.fiap.minhaestante.model;
 
+import java.util.Random;
+
 public record Resenha(Long id, String nome, String icone) {
-    
+    public Resenha(Long id, String nome, String icone){
+        this.id = Math.abs(new Random().nextLong());
+        this.nome = nome;
+        this.icone = icone;
+    }
 
 }
 
