06c123a1464482655f0f6a7add76db1ba739ee9a
thiemimi <thiemi.h.favaro@gmail.com>
2024-04-04 10:49:52 -0300

    Alterações em EstanteRepository e Resenha

--- src/main/java/br/com/fiap/minhaestante/repository/EstanteRepository.java
+++ src/main/java/br/com/fiap/minhaestante/repository/EstanteRepository.java
@@ -1,7 +1,6 @@
 package br.com.fiap.minhaestante.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.com.fiap.minhaestante.model.Estante;
 
 public interface EstanteRepository extends JpaRepository<Estante, Long> {
